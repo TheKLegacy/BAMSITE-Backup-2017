ef5ef51da9b7f9ab6ef1ad24e5bffed3bb9fa5dd
K legacy <domininkkingdiepie@gmail.com>
2018-01-25 16:17:31 -0600

    PLug in script
    A concept solution for sprites

---
+++ spriteScript.js
@@ -0,0 +1,4 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = "https://vignette.wikia.nocookie.net/animeme/images/d/d8/Ash_Ketchum_Based_On.png/revision/latest?cb=20160709053243";
+}
